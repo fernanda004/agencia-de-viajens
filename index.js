37cb6bb0c219d2c333d5b375d4c90f2284c1ff87
fernanda004 <f86869452@gmail.com>
2025-04-15 18:48:21 -0300

    arrumar o cadastrar

--- index.js
+++ index.js
@@ -2,21 +2,23 @@ const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+ 
  
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
  
-// Configuração da conexão com o banco de dados      
+// Configuração da conexão com o banco de dados
 const connection = mysql.createConnection({
-    host: 'localhost', 
-    user: 'root', 
-    password: 'root', 
-    database: 'agencia_viagens', 
-    port: 3306 
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'agencia_viagens',
+    port: 3306
 });
+ 
+ 
  
 connection.connect((err) => {
     if (err) {
@@ -25,6 +27,7 @@ connection.connect((err) => {
         console.log('Conectado ao banco de dados!');
     }
 });
+ 
  
 // Rota para cadastrar viagens
 app.post('/viagens', (req, res) => {
@@ -40,60 +43,53 @@ app.post('/viagens', (req, res) => {
 });
  
 // Rota para listar viagens
-app.get('/listar', function(req, res){
-    const listar = "SELECT * FROM viagens";
- 
-        connection.query(listar, function(err,rows){
-            if(!err){
-                console.log("Consulta realizada com sucesso!");
+app.get('/listar', (req, res) => {
+    const listar = 'SELECT * FROM viagens';
+    connection.query(listar, (err, rows) => {
+        if (!err) {
+            console.log("Consulta realizada com sucesso!");
                 res.send(`
                     <html>
                         <head>
-                    <title>Relatório de viagem</title>
+                    <title>Relatório de estoque</title>
                         </head>
                     <body>
-                        <h1>Relatório de viagem</h1>
+                        <h1>Relatório de estoque</h1>
                         <table>
                         <tr>
-                            <th>id</th>
-                            <th>destino</th>
-                            <th>data</th>
-                            <th>Valor</th>
-                            <th>vagas<th>
+                            <th>ID</th>
+                            <th>Destino</th>
+                            <th>Data Viagem</th>
+                            <th>Preço</th>
+                            <th>Vagas</th>
+                            <th>Ações</th>
                         </tr>
-                       
-                        ${rows.map(row => `
+                    ${rows.map(row => `
                         <tr>
                          <td> ${row.id} </td>
                          <td> ${row.destino} </td>
                          <td> ${row.data_viagem} </td>
                          <td> ${row.preco} </td>
                          <td> ${row.vagas} </td>
-                        <td> <a href="/excluir/${row.id} ">excluir</a>
-                         <td><a href="/editar/${row.id}">editar</a></td>                        
-                       
+                         <td>
+                            <a href="/excluir/${row.id}">Excluir<a/>
+                             <a href="/editar/${row.id}">Editar<a/>
                         </tr>
+                       
                         `).join('')}
                         </table>
-                        <a href="/">Voltar</a>
+                        <br> <button onclick="window.location.href='/'">Voltar</button>
                     </body>
  
                     </html>
                 `);
-            }else{
-                console.log("Erro no relatório de viagem", err);
-                res.send("Erro")
-            }
-        })
-});
-
-
-// Rota para a página default
-app.get("/", function(req, res){
-    res.sendFile(__dirname + "/index.html")
+        } else {
+            console.error('Erro ao listar viagens:', err);
+            res.status(500).send('Erro ao listar viagens.');
+        }
+    });
 });
  
-
 app.get('/excluir/:id', function(req,res){
     const id = req.params.id;
  
@@ -103,19 +99,20 @@ app.get('/excluir/:id', function(req,res){
             return;
         }
  
-        console.log("viagem excluída com sucesso!");
+        console.log("Viagem excluída com sucesso!");
         res.redirect('/listar');
  
     });
 });
-
+ 
+ 
 app.get('/editar/:id', function(req, res){
     const id = req.params.id; // Obtém o ID do produto a ser editado da URL
     const select = "SELECT * FROM viagens WHERE id = ?";
    
     connection.query(select, [id], function(err, rows){
         if(!err){
-            console.log("viagem encontrada com sucesso!");
+            console.log("Viagem encontrada com sucesso!");
             res.send(`
                 <html>
                     <head>
@@ -132,8 +129,9 @@ app.get('/editar/:id', function(req, res){
                             <input type="number" name="preco" value="${rows[0].preco}"><br><br>
                             <label for="vagas">Vagas:</label><br>
                             <input type="number" name="vagas" value="${rows[0].vagas}"><br><br>
-                            <input type="submit" value="cadastrar">
+                            <input type="submit" value="Cadastrar">
                         </form>
+                        <button onclick="window.location.href='/listar'">Voltar</button>
                     </body>
                 </html>`);
         }else{
@@ -143,7 +141,7 @@ app.get('/editar/:id', function(req, res){
     });
  
 });
-
+ 
 app.post('/editar/:id', function(req, res){
     const id = req.params.id; // Obtém o ID do produto a ser editado da URL
     const destino = req.body.destino; // Obtém a nova descrição do corpo da requisição
@@ -155,7 +153,7 @@ app.post('/editar/:id', function(req, res){
  
     connection.query(update, [destino, data_viagem, preco, vagas, id], function(err, result){
         if(!err){
-            console.log("viagem editada com sucesso!");
+            console.log("Viagem editada com sucesso!");
             res.redirect('/listar'); // Redireciona para a página de listagem após a edição
         }else{
             console.log("Erro ao editar a viagem ", err);
@@ -163,7 +161,13 @@ app.post('/editar/:id', function(req, res){
         }
     });
 });
-
+ 
+ 
+// Rota para a página default
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html")
+});
+ 
 // Inicia o servidor
 app.listen(8083, () => {
     console.log('Servidor rodando na url http://localhost:8083');
